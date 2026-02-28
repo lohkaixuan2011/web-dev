64497b10022fde7d78f11c2387c15dc379ec7217
lohkaixuan2011 <marcusloh2011@gmail.com>
2026-02-28 13:14:22 +0800

    added slide CSS

--- Blog/script.js
+++ Blog/script.js
@@ -1,3 +1,7 @@
+var slides = document.querySelectorAll(".slide");
+var buttons = document.querySelectorAll(".slide-btn")
+let currentSlider = 1
+
 function changeText() {
     var replaceText = document.getElementsByClassName("mini-text");
     replaceText[0].innerHTML = "Scroll to use";
@@ -6,3 +10,24 @@ function changeText() {
 
     document.getElementById("icon-down").style.display = "block";
 }
+
+//Manual images slider
+var manualNav = function(manual) {
+    slides.forEach(function (slide) {
+        slide.classList.remove("active");
+
+        buttons.forEach((btn) => {
+            btn.classList.remove("active");
+        });
+    });
+        
+    slides[manual].classList.add("active");
+    buttons[manual].classList.add("active");        
+}
+
+buttons.forEach(function(btn, i) {
+    btn.addEventListener("click", function() {
+        manualNav(i);
+        currentSlider = i;
+    })
+})
