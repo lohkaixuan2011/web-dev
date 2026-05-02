dc0e80246c4f638e97ed80f49810d708294678d4
lohkaixuan2011 <marcusloh2011@gmail.com>
2026-05-02 13:19:39 +0800

    added animation to all content

--- Blog/script.js
+++ Blog/script.js
@@ -1,5 +1,7 @@
 var slides = document.querySelectorAll(".slide");
 var buttons = document.querySelectorAll(".slider-btn")
+var btnSubmit = document.querySelector('.submit');
+var modal = document.querySelector('.modal-container');
 let currentSlider = 1
 
 function changeText() {
@@ -59,3 +61,74 @@ var repeat = function (activeClass) {
     repeater()
 }
 repeat()
+
+function changeText() {
+    var replaceText = document.getElementsByClassName("mini-text");
+    replaceText[0].innerHTML = "Scroll to use";
+
+    document.getElementById("icon-up").style.display = "none";
+
+    document.getElementById("icon-down").style.display = "block";
+}
+
+//Manual images slider
+var manualNav = function (manual) {
+    slides.forEach(function (slide) {
+        slide.classList.remove("active");
+
+        buttons.forEach((btn) => {
+            btn.classList.remove("active");
+        });
+    });
+
+    slides[manual].classList.add("active");
+    buttons[manual].classList.add("active");
+}
+
+buttons.forEach(function (btn, i) {
+    btn.addEventListener("click", function () {
+        manualNav(i);
+        currentSlider = i;
+    })
+})
+
+var repeat = function (activeClass) {
+    let active = document.getElementsByClassName("active");
+
+    let i = 1;
+    var repeater = function () {
+        setTimeout(function () {
+            [...active].forEach(function (activeSlide) {
+                activeSlide.classList.remove("active");
+            });
+
+            slides[i].classList.add("active");
+            buttons[i].classList.add("active");
+            i++
+
+
+            if (slides.length == i) {
+                i = 0;
+            }
+            if (i >= slides.length) {
+                return;
+            }
+            repeater();
+        }, 5000);
+    }
+    repeater()
+}
+repeat()
+
+if (btnSubmit) {
+    btnSubmit.addEventListener('click', function() {
+        modal.classList.add('show');
+
+        const next = document.querySelector('.next');
+        next.setAttribute('value', 'https://contact-page20102.netlify.app/index.html');
+    })
+}
+
+AOS.init({
+    once: true
+});
